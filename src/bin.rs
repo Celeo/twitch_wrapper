bece0e5d231861026854f0965b6ed554a930d11a
Celeo <celeodor@gmail.com>
2019-10-12 20:29:33 -0700

    Starting

---
+++ src/bin.rs
@@ -0,0 +1,13 @@
+use log::debug;
+use std::env;
+
+use twitch_wrapper::Twitch;
+
+fn main() {
+    kankyo::init().expect("Could not load environment");
+    env_logger::init();
+    debug!("Started");
+
+    let client_id = env::var("CLIENT_ID").expect("Missing CLIENT_ID env var");
+    let twitch = Twitch::new(&client_id);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,39 @@
+//! Rust wrappers for the Twitch API.
 
+// #![warn(missing_docs)]
+
+use reqwest::Client;
+
+// Main API wrapper.
+pub struct Twitch {
+    /// The reqwest HTTP client instance
+    client: Client,
+    /// The developer's client id from their Twitch developer apps
+    client_id: String,
+}
+
+impl Twitch {
+    /// Construct a new instance of the Twitch struct in order to access the API.
+    ///
+    /// # Arguments
+    ///
+    /// * `client_id` - your client id from the [developer console]
+    ///
+    /// [developer console]: https://dev.twitch.tv/console/apps
+    pub fn new(client_id: &str) -> Self {
+        Twitch {
+            client: Client::new(),
+            client_id: client_id.to_owned(),
+        }
+    }
+
+    /// Get the base REST API URL.
+    ///
+    /// Supports local unit testing through mockito.
+    fn base_url(&self) -> String {
+        #[cfg(not(test))]
+        return "https://api.twitch.tv/helix".to_owned();
+        #[cfg(test)]
+        return mockito::server_url();
+    }
+}

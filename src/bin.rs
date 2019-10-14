ede8ec0f8fb573b64473bd94737a5286eb795271
Celeo <celeodor@gmail.com>
2019-10-14 13:18:44 -0700

    Cleanup bin

--- src/bin.rs
+++ src/bin.rs
@@ -10,26 +10,7 @@ fn main() {
     debug!("Started");
 
     let client_id = env::var("CLIENT_ID").expect("Missing CLIENT_ID env var");
-    let twitch = Twitch::new(&client_id);
+    let _twitch = Twitch::new(&client_id);
 
-    // let streams = twitch.get_streams(2).unwrap();
-    // fs::write(
-    //     Path::new("./output.json"),
-    //     serde_json::to_string_pretty(&streams).unwrap(),
-    // )
-    // .unwrap();
-
-    // let streams: twitch_wrapper::models::streams::StreamList = twitch
-    //     .query("get", "streams", Some(&[("first", "2")]))
-    //     .unwrap();
-    // dbg!(&streams);
-
-    // let streams: Vec<twitch_wrapper::models::streams::StreamListItem> = twitch
-    //     .query_paginated("GET", "streams", None, 100, 101)
-    //     .unwrap();
-    // fs::write(
-    //     Path::new("./output.json"),
-    //     serde_json::to_string_pretty(&streams).unwrap(),
-    // )
-    // .unwrap();
+    // ...
 }
